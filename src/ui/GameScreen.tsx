693e28c09019fb3f23a74345df9f22672cb72d5c
dafnap10 <dafnapenn@gmail.com>
2026-02-23 12:37:46 +0200

    Create GameScreen component for the main game screen interface

---
+++ src/ui/GameScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GameScreen: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Alphabet Game!</h1>
+            <p>Tap on the letters and play!</p>
+        </div>
+    );
+};
+
+export default GameScreen;
